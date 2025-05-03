8fe8658fb0f9dda7108c0ce48f2c726609c5b0e2
Maycon Do Carmo <Maycongusmao.up1@gmail.com>
2025-05-03 14:22:00 -0300

    Finalizando projeto 1 conta bancaria em java Dio

--- ContaTerminal.java
+++ ContaTerminal.java
@@ -1,6 +1,29 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
 
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da conta: ");
+        int Numero = scanner.nextInt();
+
+        scanner.nextLine();
+
+        System.out.println("Por favor, insira sua agência: ");
+        String Agencia = scanner.nextLine();
+
+        System.out.println("Por favor, insira o seu nome: ");
+        String Nome = scanner.nextLine();
+
+        System.out.println("Por favor, insira o seu saldo: ");
+        float Saldo = scanner.nextFloat();
+
+        scanner.close();
+
+        System.out.println("Olá " + Nome + ", obrigado por criar uma conta em nosso branco! Sua agência é " + Agencia + ", conta " + Numero + " e seu saldo de" + Saldo + " já está disponível para saque");
+
 
 
 
